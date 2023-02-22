c3fcdafa1350b97e2155ca8c1300191508b9845f
Milclimule <99533207+miclimule@users.noreply.github.com>
2023-02-23 00:32:05 +0300

    Base

--- src/Number/ConvertionBase.java
+++ src/Number/ConvertionBase.java
@@ -2,4 +2,24 @@ package Number;
 
 public class ConvertionBase {
 
+	public String changeBase(String number, int base_a, int base_b) {
+	    int decimal_number = Integer.parseInt(number, base_a);
+	    String digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	    if (decimal_number == 0) {
+	        return "0";
+	    }
+	    String new_number = "";
+	    if (base_b == 1) {
+			for (int i = 0; i < decimal_number; i++) {
+				new_number += "1";
+			}
+			return new_number;
+		}
+	    while (decimal_number > 0) {
+	        int remainder = decimal_number % base_b;
+	        new_number = digits.charAt(remainder) + new_number;
+	        decimal_number /= base_b;
+	    }
+	    return new_number;
+	}
 }
---
+++ src/Number/Main.java
@@ -0,0 +1,10 @@
+package Number;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ConvertionBase base = new ConvertionBase();
+		System.out.println(base.changeBase("A", 16, 10));
+	}
+	
+}
