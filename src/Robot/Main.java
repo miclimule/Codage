8d81ba4a8986d67345a2c396823c93cedd084b9d
Milclimule <99533207+miclimule@users.noreply.github.com>
2023-02-23 16:04:26 +0300

    Reconnaissance de texte en anglais

--- src/Robot/Main.java
+++ src/Robot/Main.java
@@ -1,10 +1,20 @@
 package Robot;
 
+import java.io.File;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		RunMinecraft minecraft = new RunMinecraft();
-		
+	public static void main(String[] args) throws TesseractException {
+//		RunMinecraft minecraft = new RunMinecraft();
+//		ScreenCapture capture = new ScreenCapture();
+//		capture.capture("file");
+		Tesseract tesseract = new Tesseract();
+		tesseract.setDatapath("D:\\Tess4J-3.4.8-src\\Tess4J");
+		String text = tesseract.doOCR(new File("chaos1.png"));
+		System.out.println(text);
 	}
 	
 }
