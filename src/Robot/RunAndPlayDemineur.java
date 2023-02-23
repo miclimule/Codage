e6452cad0308ed7cf337aa6e917de974c7e0b1fd
Milclimule <99533207+miclimule@users.noreply.github.com>
2023-02-23 16:11:18 +0300

    init class RunAndPlayDemineur

---
+++ src/Robot/RunAndPlayDemineur.java
@@ -0,0 +1,24 @@
+package Robot;
+
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+public class RunAndPlayDemineur {
+
+	public RunAndPlayDemineur() throws Exception {
+	
+		Robot robot = new Robot();
+//		robot.mouseMove(30, 1080);
+		robot.keyPress(KeyEvent.VK_WINDOWS);
+		robot.keyRelease(KeyEvent.VK_WINDOWS);
+		robot.mouseMove(99, 133);
+		
+		Thread.sleep(500);
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        
+        Thread.sleep(500);
+	}
+	
+}
