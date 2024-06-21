005df7532e58464405f0097a7c332915d61bd941
Milclimule <99533207+miclimule@users.noreply.github.com>
2024-06-21 13:11:28 +0300

    up

--- src/Coloration/Coloration.java
+++ src/Coloration/Coloration.java
@@ -10,7 +10,7 @@ public class Coloration {
     private int numColors;
     
     enum Point {
-        A(0), B(1), C(2), D(3), E(4), F(5), G(6), H(7);
+        A(0), B(1), C(2), D(3), E(4), F(5), G(6), H(7), I(8), J(9), K(10), L(11);
 
         private final int value;
 
